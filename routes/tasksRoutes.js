ab97dfd6b766d3037b30a02c650273e5727a4e04
udaynarendra <udaynarendragudiguntla@gmail.com>
2026-07-21 17:22:38 +0530

    delete taskController.js and tasksRoutes.js

--- routes/tasksRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {addTask,deleteTask,updateTask,getAllTasks} from '../controllers/tasksController.js';
-const router = express.Router();
-router.get('/',getAllTasks)
-router.post('/add',addTask);
-router.put('/update/:id',updateTask)
-router.delete('/delete/:id',deleteTask)
-
-export default router;
